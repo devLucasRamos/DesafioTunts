957dc354b7aad84ca588c8dabda47789679a2026
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-10-05 13:08:59 -0300

    Consumindo uma API google para Google sheets

---
+++ ConsumindoAPIGoogle/ConsumindoAPIGoogle/GoogleAPI/SheetsServices.cs
@@ -0,0 +1,24 @@
+﻿using Google.Apis.Sheets.v4;
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+
+namespace GoogleAPI
+{
+    class SheetsServices
+    {
+        private const string applicationName = "APIGoogle";
+
+        public static SheetsService GetSheetsService(UserCredential credential)
+        {
+
+            var serviceInitializer = new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName
+            };
+
+            return new SheetsService(serviceInitializer);
+        }
+
+    }
+}
